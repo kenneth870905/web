e57f9d7a42b46dff257fde10a0c58e9f8d1dde2a
kenneth870905 <q359352759@gmail.com>
2020-11-19 04:24:56 +0800

    添加王老板

---
+++ 王旭/paizhao/main.js
@@ -0,0 +1,40 @@
+const { app, BrowserWindow  , BrowserView} = require('electron')
+
+
+// 热加载
+// require('electron-reloader')(module,{});
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 960,
+        height: 540,
+        webPreferences: {
+            nodeIntegration: true,
+            enableRemoteModule: true
+        },
+        //全屏
+        fullscreen :true,
+        // 隐藏标题栏
+        // frame: false,
+    })
+    //菜单
+    win.setMenuBarVisibility(false)
+    //全屏
+    // win.simpleFullScreen = true
+    win.loadFile('src/index.html')
+
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
+})
