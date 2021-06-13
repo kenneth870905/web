fc4d16ed2fc87dbdaeeed464b8313bc1c2f19b91
kenneth870905 <test@qq.com>
2021-06-14 02:27:50 +0800

    0227

---
+++ 斗鸡/douji/src/main.js
@@ -0,0 +1,16 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+import ElementPlus from 'element-plus';
+import 'element-plus/lib/theme-chalk/index.css';
+import 'element-plus/lib/theme-chalk/display.css';
+
+
+const app = createApp(App)
+app.use(ElementPlus)
+
+app.use(store).use(router).mount('#app')
+
+
